ee6eab1eb120f305cf9097bde270267d184373b7
tonovarela <tonovarela@live.com>
2022-07-13 21:41:42 -0500

    configuracion del store y del slice

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {increment,decrement,incrementByAmount} from "./store/slices/counter";
+
+function App() {	
+	const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+	return (
+		<div className="App">
+			<div className="card">
+        <h1>{counter}</h1>
+				<button onClick={() =>{ dispatch(increment()) }}>+1</button>
+        <button onClick={() =>{ dispatch(incrementByAmount(5)) }}>+5</button>
+        <button onClick={() =>{ dispatch(decrement()) }}>-</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
